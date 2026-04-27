eaf3f33da47b0ea5678a440de376ab9039153180
shivraj778 <panchalshivraj01@gmail.com>
2026-04-27 22:39:46 +0530

    three sum example

---
+++ src/main/java/multithreading/interview/ThreeSum.java
@@ -0,0 +1,38 @@
+package multithreading.interview;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] a = new int[] {-1,0,1,2,-1,-4};
+        System.out.println(ThreeSum.threeSum(a));
+    }
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list =  new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i=0;i< nums.length-2;i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i+1;
+            int right = nums.length-1;
+            while (left < right) {
+                int sum =  nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    list.add(List.of(nums[i] , nums[left] ,nums[right]));
+
+                    while (left < right && nums[left] == nums[left+1]) left++;
+                    while (left < right && nums[right] == nums[right-1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left ++;
+                } else {
+                    right --;
+                }
+            }
+        }
+
+        return list;
+    }
+}
